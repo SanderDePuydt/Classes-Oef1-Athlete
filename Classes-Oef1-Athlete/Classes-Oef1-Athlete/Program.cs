c6d9e3ca63e9fdf6cb082fb6d53e5940fa5a1416
Sander De Puydt <pxldemo@gmail.com>
2025-11-06 10:15:04 +0100

    init project

---
+++ Classes-Oef1-Athlete/Classes-Oef1-Athlete/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Classes_Oef1_Athlete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
